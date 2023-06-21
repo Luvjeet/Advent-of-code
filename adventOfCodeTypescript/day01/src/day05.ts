cf1071095547bace1a8ea1e86bab0f2da583cf6f
Luvjeet <luzag1402@gmail.com>
2023-06-21 13:24:05 +0530

    feat: day05 done

--- adventOfCodeTypescript/day01/src/day05.ts
+++ adventOfCodeTypescript/day01/src/day05.ts
@@ -2,7 +2,6 @@ import fs from "fs";
 
 
 const stack = fs.readFileSync("src/stack.txt", "utf8").split("\n");
-stack.pop();
 
 const input = fs.readFileSync("src/moves.txt").toString();
 
@@ -16,3 +15,26 @@ for (let row of stack) {
     }
 }
 
+
+
+let start = 0, end = 10;
+console.log(start + Math.floor((end - start) / 2));
+
+
+//function leftPad(str: string, len: number, ch: any): string[] {
+//    return new Array(len - str.length).fill(!ch && ch !== 0 ? " " : ch);
+//}
+//
+//function run(fn: Function, count: number, ...args: any) {
+//    const start = performance.now();
+//    for (let i = 0; i < count; ++i) {
+//        fn.apply(null, args);
+//    }
+//    return performance.now() - start;
+//}
+//
+//
+//[10, 100, 1000, 10_000].forEach((x: number) => {
+//    console.log("Leftpad=> ", run(leftPad, x, "foo", x));
+//})
+//
